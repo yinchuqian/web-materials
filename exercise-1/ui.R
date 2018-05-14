ac0e1fbfed65f67cc89d4e3aaf8c48106191d704
Anni Yan <anniyan@D-10-18-255-27.dhcp4.washington.edu>
2018-05-14 14:00:58 -0700

    added style

--- exercise-1/ui.R
+++ exercise-1/ui.R
@@ -10,9 +10,10 @@ shinyUI(navbarPage(theme = shinytheme("sandstone"),
     titlePanel("Electoral College Votes"),
     # Create sidebar layout
     sidebarLayout(
-
+    
       # Side panel for controls
       sidebarPanel(
+        tags$h2("This is an interactive map"),
 
         # Input to select variable to map
         selectInput(
@@ -23,14 +24,29 @@ shinyUI(navbarPage(theme = shinytheme("sandstone"),
             "Electoral Votes" = "votes",
             "Votes / Population" = "ratio"
           )
-        )
+        ),
+        tags$br(),
+        HTML("<strong>Wow, I'm blown away!</strong>"),
+        tags$div(class = "sidebar",
+                 tags$blockquote("I'm a floating blockquote!"))
       ),
 
       # Main panel: display plotly map
       mainPanel(
-        plotlyOutput("map")
+        plotlyOutput("map"),
+        tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+        tags$div(class = "summary", checked = NA,
+          tags$p("This map shows the electoral college votes by states. The darker
+               the color, the higher the number of votes. California has 
+                 the highest votes.")
+        ),
+        tags$a(href = "https://www.archives.gov/federal-register/electoral-college/about.html",
+               "for more information")
       )
     )
+  ),
+  tabPanel("other",
+    tags$h1("This page is complietely empty")
   )
 
   # Create a tabPanel to show your scatter plot
